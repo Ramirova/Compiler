1025557f1850a497ac4670d922014ef6ce84b56d
AnastasiaVorton <anastasia.vorton@gmail.com>
2018-11-30 18:15:30 +0300

    Create SymbolTableGenerator.

---
+++ SymbolTableGenerator.py
@@ -0,0 +1,87 @@
+from HelloListener import HelloListener
+
+class SymbolTableGenerator(HelloListener):
+
+    def enterProgram(self, ctx):
+        pass
+
+    def enterSimpleDeclaration(self, ctx):
+        pass
+
+    def enterVariableDeclaration(self, ctx):
+        pass
+
+    def enterTypeDeclaration(self, ctx):
+        pass
+
+    def enterType(self, ctx):
+        pass
+
+    def enterPrimitiveType(self, ctx):
+        pass
+
+    def enterUserType(self, ctx):
+        pass
+
+    def enterRecordType(self, ctx):
+        pass
+
+    def enterArrayType(self, ctx):
+        pass
+
+    def enterStatement(self, ctx):
+        pass
+
+    def enterAssignment(self, ctx):
+        pass
+
+    def enterRoutineCall(self, ctx):
+        pass
+
+    def enterWhileLoop(self, ctx):
+        pass
+
+    def enterForLoop(self, ctx):
+        pass
+
+    def enterRange(self, ctx):
+        pass
+
+    def enterIfStatement(self, ctx):
+        pass
+
+    def enterRoutineDeclaration(self, ctx):
+        pass
+
+    def enterParameters(self, ctx):
+        pass
+
+    def enterParameterDeclaration(self, ctx):
+        pass
+
+    def enterBody(self, ctx):
+        pass
+
+    def enterExpression(self, ctx):
+        pass
+
+    def enterRelation(self, ctx):
+        pass
+
+    def enterSimple(self, ctx):
+        pass
+
+    def enterFactor(self, ctx):
+        pass
+
+    def enterSummand(self, ctx):
+        pass
+
+    def enterPrimary(self, ctx):
+        pass
+
+    def enterModifiablePrimary(self, ctx):
+        pass
+
+    def enterEos(self, ctx):
+        pass
