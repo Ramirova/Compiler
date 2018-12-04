da2de013e967bd1a44c036b68f3fbca9ddaad65b
Georgiy Dzesov <g.dzesov@gmail.com>
2018-12-04 19:16:12 +0300

    fix C code generation

--- C_makefile.py
+++ C_makefile.py
@@ -1,6 +1,9 @@
 """
 Class writes to C file the result of code generation in appropriate order (type definitions, declarations, main method)
 """
+from SymbolTable import *
+
+
 class C_makefile:
     def __init__(self, typedef_queue, main_queue, routines):
         """
@@ -19,7 +22,7 @@ class C_makefile:
         :return:
         """
         file = open("c_file.c", "w+")
-        file.write("#include <stdio.h>\n")
+        file.write("#include <stdio.h>\n#include <stdbool.h>\n")
 
         for typedef in self.typedef_queue:
             file.write(typedef)
@@ -40,12 +43,32 @@ class C_makefile:
         main = ""
         main += "int main (int argc, char *argv[]) {\n"
         main += "if argc > 0 {\n"
-        main += "switch(argv[0]) {\n"
+        main += "switch(argv[1]) {\n"
 
         # main += "const char *routines[" + len(self.routines) + "] = { "
         for i in range(len(self.routines)):
-            main += "case " + self.routines[i] + ":\n"
-            main += self.routines[i] + "(argv)\n"
+            main += "case \"" + self.routines[i] + "\":\n"
+            main += self.routines[i]
+            print(self.routines[i])
+            if SymbolTable.root_table.get_routine_info(routine_name=self.routines[i]).parameters is None:
+                main += '()\n'
+            else:
+                main += "("
+                parameters = SymbolTable.root_table.get_routine_info(routine_name=self.routines[i]).parameters
+                print(parameters)
+                for j in range(len(parameters)):
+                    main += self.get_param_type(parameters[j]) + "argc[" + str(j + 2) + "], "
+                main = main[:-2]
+                main += ");\n"
             main += "break;\n"
         main += "default:\n break;\n}\n}"
         return main
+
+    def get_param_type(self, type_id):
+        if type_id == 1:
+            return "(int) "
+        elif type_id == 2:
+            return "(double) "
+        elif type_id == 3:
+            return "(bool) "
+        return ""
