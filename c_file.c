8b8dc8729d1415a3fdc23cf371ad9beed07986d3
Rozaliya <rozaliya.amirova@gmail.com>
2018-12-05 17:55:24 +0300

    printf function

--- c_file.c
+++ c_file.c
@@ -4,6 +4,14 @@
 
 int bubble_sort() {
 int b;
-printf ("int, );
+printf ("%d", 1+1);
 return b;}
 
+int main (int argc, char *argv[]) {
+if (argc > 1) {
+if (strcmp(argv[1], "bubble_sort") > 0) {
+bubble_sort();
+}
+
+}
+}
